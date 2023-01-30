f92ecc7f36a682ff234aa06b848d585b3aa1f6ee
hannmx <mxkud@mail.ru>
2023-01-30 14:03:43 +0300

    Исправление программы для вычисления факториала

--- MyPy7.py
+++ MyPy7.py
@@ -2,10 +2,19 @@
 # N и выдает набор произведений чисел от 1 до N
 
 n = int(input('Введите число для вычисления факториала: '))
-f = n
-factor = 1
-while n > 1:
-    factor *= n
-    n -= 1
  
-print(f'Факториал числа {f} = {factor}')
+factorial = 1
+print('[', end = '')
+for i in range(1, n + 1):
+    if i != n:
+        factorial *= i
+        print(f'{factorial}, ', end = '')    
+    else:
+        factorial *= i
+        print(f'{factorial}', end = '')
+
+print(']', end = '')
+print(f'\nФакториал числа {n} равен: {factorial}')
+
+ 
+
